2b7e301d5d57cb6900d1f6b61bf01d42e1eecb7b
Chia hao Shen <hoa@Chiade-MacBook-Pro.local>
2017-05-21 12:13:24 +0800

    add Progress to be updated

---
+++ src/alg.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sklearn
+import argparse
+import numpy as np
+import csv
+
+def read_csv_file(filename):
+    lab = []
+    feats = []
+    with open(filename, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            feats.append(list(map(float,row[:-1])))
+            lab.append(int(row[-1]))
+    return feats, lab
+
+
+def main():
+    
+    return
+
+
+if __name__ == '__main__':
+
+    main()
