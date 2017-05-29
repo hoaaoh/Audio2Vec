b812d87e2065d457f7c7827c1146202a0432384e
hoaaoh <b8i3054755@hotmail.com>
2017-05-29 14:18:23 +0800

    update gen_len.py

--- src/gen_len.py
+++ src/gen_len.py
@@ -5,7 +5,6 @@ import argparse
 
 FLAG = None
 
-
 def read_csv(fn):
     feats = []
     labs = []
@@ -24,7 +23,7 @@ def gen_len(csvlist):
       length_list: the list that records the length of each mfcc occurance
     '''
     dim = FLAG.feat_dim
-    length = csvlist[0]/dim
+    length = int(len(csvlist[0])/dim)
     len_list = []
     for i in csvlist:
         each_len = length
@@ -40,19 +39,27 @@ def gen_len(csvlist):
             each_len -= 1
     return len_list
 
+def write_file(len_list, out_fn):
+    with open(out_fn, 'w') as f:
+        for i in len_list[:-1]:
+            f.write(str(i)+'\n')
+        f.write(str(len_list[-1]))
+    return 
+
 def main():
-    
     feats, labs = read_csv(FLAG.csvfile)
     len_list = gen_len(feats)
-
-    return
+    write_file(len_list, FLAG.out_fn)
+    return 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
       description='generate the length list of each utturance')
-    parser.add_argument('feat_dim',type=int, default=39,
+    parser.add_argument('--feat_dim',type=int, default=39,
         help='the mfcc dimension')
     parser.add_argument('csvfile',
         help='the file to be parsed')
+    parser.add_argument('out_fn',
+        help='the output filename')
     FLAG = parser.parse_args()
     main()
