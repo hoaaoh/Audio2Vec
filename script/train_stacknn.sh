cd039dc08f1bc279dfcc3f1f3649860dd0173a83
hoaaoh <b8i3054755@hotmail.com>
2017-10-01 15:56:24 +0800

    update stack rnn training script

--- script/train_stacknn.sh
+++ script/train_stacknn.sh
@@ -2,9 +2,9 @@
 
 [ -f path.sh ] && . path.sh || exit 1
 
-if [ $# != 2 ];
+if [ $# != 3 ];
 then
-    echo "usage: ./script/train_stckrnn.sh <STACK_NUM> <GPU_NUM>"
+    echo "usage: ./script/train_stckrnn.sh <STACK NUM> <HIDDEN DIM> <BATCHSIZE> <GPU_NUM>"
     exit 1
 fi
 
@@ -13,10 +13,15 @@ log_dir=/home_local/hoa/exp/log
 model_dir=/home_local/hoa/exp/model
 feat_dir=/home_local/hoa/libri_feat
 stack_num=$1
+hidden_dim=$2
+batch_size=$3
+export CUDA_VISIBLE_DEVICES=$4
 
-export CUDA_VISIBLE_DEVICES=$2
+
 mkdir -p $log_dir/$dim $model_dir/$dim
-
-audio2vec_train.py --init_lr=1 --decay_rate=1000 --hidden_dim=100 --stack_num=$stack_num --batch_size=500 --max_step=100000  \
-	$log_dir/$dim $model_dir/$dim $feat_dir/train_AE.scp
+mkdir -p $log_dir/$dim/"$stack_num"_"$batch_size" $model_dir/$dim/"$stack_num"_"$batch_size"
+log_dir=$log_dir/$dim/"$stack_num"_"$batch_size"
+feat_dir=$model_dir/$dim/"$stack_num"_"$batch_size"
+audio2vec_train.py --init_lr=1 --decay_rate=1000 --hidden_dim=$hidden_dim --stack_num=$stack_num --batch_size=$batch_size --max_step=100000  \
+	$log_dir $model_dir $feat_dir/train_AE.scp
 
