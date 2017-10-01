7db71cedfc70e65c02d328ff769f9cd02878ed76
hoaaoh <b8i3054755@hotmail.com>
2017-10-01 15:45:49 +0800

    update stack rnn training script

--- script/train_stacknn.sh
+++ script/train_stacknn.sh
@@ -2,7 +2,7 @@
 
 [ -f path.sh ] && . path.sh || exit 1
 
-if [ $# != 3 ];
+if [ $# != 2 ];
 then
     echo "usage: ./script/train_stckrnn.sh <STACK_NUM> <GPU_NUM>"
     exit 1
@@ -12,9 +12,9 @@ dim=100
 log_dir=/home_local/hoa/exp/log
 model_dir=/home_local/hoa/exp/model
 feat_dir=/home_local/hoa/libri_feat
-stack_num=$2
+stack_num=$1
 
-export CUDA_VISIBLE_DEVICES=$3
+export CUDA_VISIBLE_DEVICES=$2
 mkdir -p $log_dir/$dim $model_dir/$dim
 
 audio2vec_train.py --init_lr=1 --decay_rate=1000 --hidden_dim=100 --stack_num=$stack_num --batch_size=500 --max_step=100000  \
