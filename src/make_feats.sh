3388fc03321f3981d4ffb27fc76568f56da19a0b
grtzsohalf <piercing.zs0half@gmail.com>
2018-02-18 22:35:22 +0800

    a little update

--- src/make_feats.sh
+++ src/make_feats.sh
@@ -3,7 +3,7 @@
 [ -f path.sh ] && . ./path.sh
 path=/home/grtzsohalf/Audio2Vec
 #feat_dir=/home_local/grtzsohalf/yeeee/French
-feat_dir=/nfs/Mazu/grtzsohalf/yeeee/German
+feat_dir=/nfs/Mazu/grtzsohalf/yeeee/English
 
 gram_num=2
 
@@ -16,6 +16,6 @@ if [ ! -f $feat_dir/feats/extracted ]; then
   [ -f $feat_dir/filtered_prons ] && rm -rf $feat_dir/filtered_prons
   mkdir -p $feat_dir/feats
   python3 $path/src/get_feat.py --gram_num=$gram_num $feat_dir/all_prons $feat_dir/cmvned_feats.ark \
-    $feat_dir/feats $feat_dir/filtered_prons 2> get_feat.log
+    $feat_dir/feats $feat_dir/filtered_prons
   echo 1 > $feat_dir/feats/extracted
 fi
